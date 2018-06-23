d8ecf338174274215b886daee212a4307b5a34b2
Emanuele Fumagalli <emanuelef@gmail.com>
2018-06-23 19:41:49 +0100

    added date selector and update logic

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
+from datetime import datetime as dt
 import plotly.plotly as py
 from plotly import graph_objs as go
 from plotly.graph_objs import *
@@ -10,67 +11,89 @@ from flask_cors import CORS
 import pandas as pd
 import numpy as np
 import os
+import time
 
 app = dash.Dash('AirTrApp')
 server = app.server
 
 mapbox_access_token = 'pk.eyJ1IjoiZW1hZnVtYSIsImEiOiJjamh1ZGVoZGowbGExM3duMDkwMnhtNDhiIn0.xgW6mtfaTEgFNw8jC6i_Yw'
 
-df = pd.read_json(
-    'http://ec2-18-130-36-119.eu-west-2.compute.amazonaws.com/all?from=1529335903&to=1529350308'
-)
-
-df = pd.DataFrame(list(df["items"]))
-
-df["Date/Time"] = pd.to_datetime(df['time'], unit='s')
-df.index = df["Date/Time"]
-df.drop("Date/Time", 1, inplace=True)
-
-df = df.drop(df.columns[[0, 1, 2, 4, 5, 8, 9, 10, 11, 12, 13, 14]], axis=1)
-df = df.rename(columns={'latitude': 'Lat', 'longitude': 'Lon', 'galtM': 'Alt'})
-
 app.layout = html.Div(
     [
-        dcc.Graph(
-            id='life-exp-vs-gdp',
-            figure={
-                'data': [
-                    go.Scattermapbox(
-                        lat=df['Lat'],
-                        lon=df['Lon'],
-                        text=df['Alt'],
-                        hoverinfo="lat+lon+text",
-                        mode='markers',
-                        opacity=0.7,
-                        marker=Marker(
-                            color=df['Alt'],
-                            colorscale=[[0, "#F4EC15"], [0.04167, "#DAF017"], [
-                                0.0833, "#BBEC19"
-                            ], [0.125, "9DE81B"], [0.1667, "#80E41D"], [
-                                0.2083, "#66E01F"
-                            ], [0.25, "#4CDC20"], [0.292, "#34D822"], [
-                                0.333, "#24D249"
-                            ], [0.375, "#25D042"], [0.4167, "#26CC58"], [
-                                0.4583, "#28C86D"
-                            ], [0.50, "#29C481"], [0.54167, "#2AC093"],
-                                        [0.5833, "#2BBCA4"], [1.0, "#613099"]],
-                            opacity=0.5,
-                        ))
-                ],
-                'layout':
-                go.Layout(
-                    autosize=True,
-                    hovermode='closest',
-                    mapbox=dict(
-                        accesstoken=mapbox_access_token,
-                        bearing=0,
-                        center=dict(lat=51.443874, lon=-0.342588),
-                        pitch=0,
-                        zoom=10),
-                )
-            }),
+        html.H2('Flights passages'),
+        dcc.DatePickerSingle(id='date-picker-single', date=dt(2018, 6, 10)),
+        dcc.Graph(id='passage-flights'),
+        dcc.RangeSlider(
+            marks={i: 'H {}'.format(i)
+                   for i in range(0, 24)},
+            min=0,
+            max=24,
+            value=[0, 24])
     ],
-    style={"padding-top": "10px"})
+    style={'margin': 'auto auto'})
+
+
+@app.callback(
+    dash.dependencies.Output('passage-flights', 'figure'),
+    [dash.dependencies.Input('date-picker-single', 'date')])
+def update_figure(selected_day):
+    print(selected_day)
+    selected_day=selected_day.split()[0]
+
+    pattern = '%Y-%m-%d'
+    epochStart = int(time.mktime(time.strptime(selected_day, pattern)))
+    epochEnd = epochStart + 60 * 60 * 24
+
+    print(epochStart)
+    print(epochEnd)
+
+    urlRequest = 'http://ec2-18-130-36-119.eu-west-2.compute.amazonaws.com/all?from={}&to={}'.format(epochStart, epochEnd)
+
+    print(urlRequest)
+
+    df = pd.read_json(urlRequest)
+
+    df = pd.DataFrame(list(df["items"]))
+
+    df["Date/Time"] = pd.to_datetime(df['time'], unit='s')
+    df.index = df["Date/Time"]
+    df.drop("Date/Time", 1, inplace=True)
+
+    df = df.drop(df.columns[[0, 1, 2, 4, 5, 8, 9, 10, 11, 12, 13, 14]], axis=1)
+    df = df.rename(columns={
+        'latitude': 'Lat',
+        'longitude': 'Lon',
+        'galtM': 'Alt'
+    })
+    return {
+        'data': [
+            go.Scattermapbox(
+                lat=df['Lat'],
+                lon=df['Lon'],
+                text=df['Alt'],
+                hoverinfo="lat+lon+text",
+                mode='markers',
+                opacity=0.7,
+                marker=Marker(
+                    color=df['Alt'],
+                    colorscale='Hot',
+                    opacity=0.5,
+                ))
+        ],
+        'layout':
+        go.Layout(
+            autosize=True,
+            hovermode='closest',
+            mapbox=dict(
+                accesstoken=mapbox_access_token,
+                bearing=0,
+                center=dict(lat=51.443874, lon=-0.342588),
+                pitch=0,
+                style='dark',
+                zoom=9),
+        )
+    }
+
 
 external_css = [
     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
